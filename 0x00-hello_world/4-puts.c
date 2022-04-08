dc5746a585cc8da23e56e972380a9527b24e81a8
Moyin <oyemoyin26@gmail.com>
2022-04-07 17:04:47 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** 
+ *main - Entry point
+ *Return: Always 0 (successs)
+ */
+int main(void)
+{
+	  puts("\"Programming is like building a multilinigual puzzle");
+	  return (0);
+}
